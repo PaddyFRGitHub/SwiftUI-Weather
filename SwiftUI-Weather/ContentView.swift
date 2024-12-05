33e15f8eb1b692ec479903b619095fa5c3ad66b6
PaddyFRGitHub <ruddenpatrick@gmail.com>
2024-12-05 23:21:31 +0000

    Second commit

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -8,15 +8,125 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isNight = false
+    
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
-                           startPoint: .topLeading,
-                           endPoint: .bottomTrailing)
-                .edgesIgnoringSafeArea(.all)
+            BackgroundView(isNight: $isNight)
+            VStack {
+                CityTextView(cityName: "London, United Kingdom")
+                
+                MainWeatherStatusView(imageName: isNight ? "moon.stars.fill" : "cloud.sun.fill", temperature: 24)
+                
+                HStack(spacing: 20) {
+                    WeatherDayView(dayOfWeek: "Mon",
+                                   imageName: "cloud.sun.fill",
+                                   temperature: 22)
+                    
+                    WeatherDayView(dayOfWeek: "Tue",
+                                   imageName: "cloud.sun.rain.fill",
+                                   temperature: 22)
+                    
+                    WeatherDayView(dayOfWeek: "Wed",
+                                   imageName: "cloud.hail.fill",
+                                   temperature: 22)
+                    
+                    WeatherDayView(dayOfWeek: "Thu",
+                                   imageName: "cloud.sun.fill",
+                                   temperature: 22)
+                    
+                    WeatherDayView(dayOfWeek: "Fri",
+                                   imageName: "cloud.bolt.rain.fill",
+                                   temperature: 22)
+                }
+                Spacer()
+                
+                Button {
+                    isNight.toggle()
+                } label: {
+                    WeatherButton(title: "Change Day Time",
+                                  textColor: .blue,
+                                  backgroundColor: .white)
+                }
+                
+                Spacer()
+            }
         }
     }
     }
 #Preview {
     ContentView()
 }
+
+
+struct WeatherDayView: View {
+    
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack {
+            Text(dayOfWeek)
+                .foregroundColor(.white)
+                .font(.system(size: 16, weight: .medium, design: .default))
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            Text("\(temperature)°")
+                .foregroundColor(.white)
+                .font(.system(size: 16, weight: .medium, design: .default))
+        }
+    }
+}
+
+struct BackgroundView: View {
+    
+    @Binding var isNight: Bool
+    
+    var body: some View {
+        
+//        LinearGradient(gradient: Gradient(colors: [isNight ? .black : .blue, isNight ? .gray : Color("Light Blue")]),
+//                       startPoint: .topLeading,
+//                       endPoint: .bottomTrailing)
+//        .ignoresSafeArea()
+        
+        ContainerRelativeShape()
+            .fill(isNight ? Color.black.gradient : Color.blue.gradient)
+            .ignoresSafeArea()
+    }
+}
+struct CityTextView: View {
+    var cityName: String
+    
+    var body: some View {
+        Text(cityName)
+            .font(.system(size: 32, weight: .medium, design: .default))
+            .foregroundColor(.white)
+    }
+}
+
+struct MainWeatherStatusView: View {
+    
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 180, height: 180)
+            
+            Text("\(temperature)°")
+                .font(.system(size: 70, weight: .medium))
+                .foregroundColor(.white)
+        }
+        .padding(.bottom, 50)
+    }
+}
+
---
+++ SwiftUI-Weather/WeatherButton.swift
@@ -0,0 +1,25 @@
+//
+//  WeatherButton.swift
+//  SwiftUI-Weather
+//
+//  Created by Patrick Rudden on 04/12/2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct WeatherButton: View {
+    
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor.gradient)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(25)
+    }
+}
